78f1845b537c24938714a5fa234c8d4df316dd96
Chinmay Kulthe <programmingdnyan2422@gmail.com>
2021-02-06 15:38:39 +0530

    c codes

---
+++ tut_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int a = 0;
+    printf("Enter a number :\n");
+    scanf("%d",&a);
+    printf("User entered number is %d",a);
+}
---
+++ tut_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a = 45;
+    float b = 56.7;
+    double c = 567.66;
+    char d = 'A';
+    printf("Int = %d\n",a);
+    printf("Float = %f\n",b);
+    printf("Double = %lf\n",c);
+    printf("Char = %c",d);
+    return 0;
+}
